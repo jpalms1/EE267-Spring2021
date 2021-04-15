bc1be6088e1c64d8f0ed17de7ac775fd2c007df5
unknown <jasminp@stanford.edu>
2021-04-15 00:15:05 -0700

    Section 2.2
    Implemented Phong Shading Model

--- homework2/js/shaders/fShaderPhong.js
+++ homework2/js/shaders/fShaderPhong.js
@@ -53,6 +53,8 @@ uniform vec3 ambientLightColor;
 
 
 void main() {
+	// Aggregate sspecular and diffuse components
+	vec3 fColorSum;
 
 	// Compute ambient reflection
 	vec3 ambientReflection = material.ambient * ambientLightColor;
@@ -60,6 +62,45 @@ void main() {
 	vec3 fColor = ambientReflection;
 
 	gl_FragColor = vec4( fColor, 1.0 );
+
+	// Set vertex position
+	vec3 positionView3 = fragPosCam;
+
+	// Re-normalize normalCam
+	vec3 normalView = normalize( normalCam );
+
+	// Attenuation parameters
+	float kc = attenuation[0];
+	float kl = attenuation[1];
+	float kq = attenuation[2];
+
+	for (int i = 0; i < NUM_POINT_LIGHTS; i++)
+	{
+		// transform light position into view space 
+		vec4 lightPositionView = viewMat * vec4( pointLights[i].position , 1.0);
+		vec3 lightPositionView3 = vec3( lightPositionView );
+
+		// Compute diffuse term
+		float diffuseFactor = max( dot( normalView, -normalize( lightPositionView3 )) , 0.0);
+
+		// Calculate distance between light source and vertex
+		float d = length( positionView3 - lightPositionView3 );
+
+		float attenuationCoeff = 1.0/(kc + kl*d + kq*d*d);
+
+		//Specular parameters
+		vec3 L = -normalize( lightPositionView3 );
+		vec3 N = normalView;
+		vec3 R = 2.0*dot(N, L)*N - L;
+		vec3 V = -normalize( positionView3 );
+
+		float specularFactor = pow( max( dot(R, V), 0.0), material.shininess);
+
+		// Add point light sources to vColor -- sigma( attentuation*(specular +diffuse) )
+		fColorSum +=  attenuationCoeff*((specularFactor * material.specular * pointLights[i].color) + (diffuseFactor * material.diffuse * pointLights[i].color));
+	}
+	// Total fColor -- ambient + sigma( attentuation*(specular + diffuse) )
+	gl_FragColor.rgb =  ambientReflection + fColorSum;
 
 }
 ` );
